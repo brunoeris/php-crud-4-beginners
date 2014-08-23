6b5e27a19d0a110492543541070970769e5d7ec4
Bruno Eris <brunoeriss@gmail.com>
2014-08-23 15:17:57 -0300

    Pastas e esqueletos
    bootstrap + jquery

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<title>Crud Simples PHP</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+</head>
+
+<body>
+	
+	
+
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
